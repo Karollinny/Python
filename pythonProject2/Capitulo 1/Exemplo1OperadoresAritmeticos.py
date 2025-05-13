886790d9675b044cb6b0f4a1b33a01f3941fdba3
Allan <allan.bento.santso@escola.pr.gov.br>
2025-05-13 11:32:19 -0300

    Capitulo 1 exemplo 1

---
+++ pythonProject2/Capitulo 1/Exemplo1OperadoresAritmeticos.py
@@ -0,0 +1,14 @@
+# Operador de soma
+soma = 1 + 2
+
+# Operador de subtracao
+subtracao = 2 - 1
+
+# Operador de multiplicacao
+multiplicacao = 2 * 1
+
+# Operdor de divisão
+divisão = 2 / 1
+
+# Operador de exponenciacao (potencia)
+exponenciacao = 2 ** 1
