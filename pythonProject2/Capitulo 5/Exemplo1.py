7f7a289c0dd3cc864dc0b1e4b89807a33a3ac097
ANA CAROLINE DE MATOS ANDRADE <ana.matos.andrade@escola.pr.gov.br>
2025-09-16 11:48:56 -0300

    Capitulo 5

---
+++ pythonProject2/Capitulo 5/Exemplo1.py
@@ -0,0 +1,13 @@
+def valinLogin(nome, senha):
+    if(nome == "Jose" and senha == "senha123"):
+        return print("Seja bm vindo", nome, senha)
+    else:
+        return print("Senha ou login invalidos")
+
+
+print("=== Digite seu nome ===")
+nome = input()
+print("=== Digite sua senha ===")
+senha = input()
+
+valinLogin(nome, senha)
